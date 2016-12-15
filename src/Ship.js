527a1221fc7bd9691daf1e34c0b54302473244f5
Đặng Xuân Quang <xquang.dang@gmail.com>
2016-12-15 15:38:22 +0700

    -add world, moving ship

--- src/Ship.js
+++ src/Ship.js
@@ -19,28 +19,32 @@ prototype.Container_initialize = prototype.initialize;
 
 prototype.initialize = function () {
     this.Container_initialize();
-    this.x = 400;
-    this.y = 240;
+    this.x = 0;
+    this.y = 0;
     this.vX = 0;
     this.vY = 0;
     this.speed = 100;
 };
 
+var browserW = $(window).innerWidth()/2;//window.innerWidth ? window.innerWidth : (document.documentElement.clientWidth ? document.documentElement.clientWidth : document.body.clientWidth);// $(window).innerWidth();
+var browserH = $(window).innerHeight()/2;//window.innerHeight ? window.innerHeight : (document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight);//$(window).innerHeight();
+
+
 prototype.update = function (event) {
     var deltaTime = event.delta/1000;
     var mousePos = Global.getInstance().mouseHelper.getMousePos();
-    var lenghX = Math.sqrt((mousePos.x - prototype.x)*(mousePos.x - prototype.x) + (mousePos.y - prototype.y)*(mousePos.y - prototype.y));
-    var lenghX0 = Math.sqrt((mousePos.x - prototype.x)*(mousePos.x - prototype.x));
+    var lenghX = Math.sqrt((mousePos.x - browserW)*(mousePos.x - browserW) + (mousePos.y - browserH)*(mousePos.y - browserH));
+    var lenghX0 = Math.sqrt((mousePos.x - browserW)*(mousePos.x - browserW));
 
     var angle = Math.acos(lenghX0/lenghX);
-    if (mousePos.x < prototype.x) {
-        if (mousePos.y < prototype.y) {
+    if (mousePos.x < browserW) {
+        if (mousePos.y < browserH) {
             angle = angle +  Math.PI;
         } else {
             angle = Math.PI - angle;
         }
     } else {
-        if (mousePos.y < prototype.y) {
+        if (mousePos.y < browserH) {
             angle = Math.PI*2 - angle;
         } else {
 
@@ -51,7 +55,10 @@ prototype.update = function (event) {
     //accelerate
     this.vX = Math.cos(angle);
     this.vY = Math.sin(angle);
-    console.log(this.vX + ' ' + this.vY);
-    prototype.x += this.vX * prototype.speed * deltaTime;
-    prototype.y += this.vY * prototype.speed * deltaTime;
+    //console.log(this.vX + ' ' + this.vY);
+    var tempX = prototype.x + this.vX * prototype.speed * deltaTime;
+    var tempY = prototype.y + this.vY * prototype.speed * deltaTime;
+
+    prototype.x = (tempX < 2000 && tempX > -2000)? tempX : (tempX < 0? -2000: 2000);
+    prototype.y = (tempY < 2000 && tempY > -2000)? tempY : (tempY < 0? -2000: 2000);
 };
---
+++ src/World.js
@@ -0,0 +1,61 @@
+var WORLD_SIZE_PIXEL = 4000;
+
+function World() {
+    prototype.initialize();
+}
+
+var prototype = World.prototype = new createjs.Container();
+
+prototype.Container_initialize = prototype.initialize;
+
+prototype.initialize = function () {
+    this.Container_initialize();
+    this.x = 0;
+    this.y = 0;
+};
+
+prototype.addShip = function (ship) {
+    this.ship=ship;
+    this.addChild(ship);
+};
+
+prototype.addWorldGrid = function () {
+    var worldLine = new createjs.Shape();
+
+    var g = worldLine.graphics;
+    g.clear();
+    g.setStrokeStyle(1);
+    g.beginStroke("#007300");
+
+    for (var i = -WORLD_SIZE_PIXEL / 2; i <= WORLD_SIZE_PIXEL / 2; i+=500){
+        g.moveTo(-WORLD_SIZE_PIXEL / 2, i);	//nose
+        g.lineTo(WORLD_SIZE_PIXEL / 2, i);	//rfin
+
+        g.moveTo(i, -WORLD_SIZE_PIXEL / 2);	//nose
+        g.lineTo(i, WORLD_SIZE_PIXEL / 2);	//rfin
+    }
+
+    g.closePath(); // nose
+    this.addChild(worldLine);
+    //world.cache(-2000,-2000, 4000, 4000);
+};
+
+
+prototype.initWorld = function () {
+    this.addWorldGrid();
+};
+
+prototype.makeCameraAroundShip = function (x, y) {
+    var browserW = $(window).innerWidth();
+    var browserH = $(window).innerHeight();
+    if (this.ship.x > -2000 && this.ship.x < 2000) {
+        this.x = -this.ship.x;
+    }
+    if (this.ship.y > -2000 && this.ship.y < 2000) {
+        this.y = -this.ship.y;
+    }
+};
+
+prototype.update = function (event) {
+     this.makeCameraAroundShip();
+};
