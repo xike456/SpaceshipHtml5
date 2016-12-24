97e6f19367c0c7ed3a79c3881799887c13aa920b
mpnguyen <minhphu.it@gmail.com>
2016-12-24 16:34:11 +0700

    Slow down bullet when shooting

--- src/models/MachineGun.js
+++ src/models/MachineGun.js
@@ -11,6 +11,8 @@
         this.gun.regY = Constants.PIECE_HEIGHT/2 + Constants.PIECE_HEIGHT / 4;
         this.gun.rotation = 0;
         this.addChild(this.gun);
+
+        this.fireDelay = Constants.MACHINE_FIRE_DELAY;
     }
 
     MachineGun.prototype = Object.create(Gun.prototype);
@@ -43,13 +45,22 @@
         angle = angle * 180 / (Math.PI) + 90 - this.parent.rotation;
         this.gun.rotation = angle;
 
+        this.fireDelay += event.delta/1000;
         if (Global.getInstance().keyboardHelper.isKeyDown(Constants.KEYCODE_W)) {
-            var posOnShip = this.gun.localToGlobal(this.gun.x + Constants.PIECE_WIDTH / 2, this.gun.y - Constants.PIECE_HEIGHT / 4);
-            var posOnWorld = Global.getInstance().world.globalToLocal(posOnShip.x, posOnShip.y);
-            var bullet = new BulletMachine(posOnWorld.x, posOnWorld.y, angle + this.parent.rotation);
-            Global.getInstance().listBullet.push(bullet);
-            Global.getInstance().world.addChild(bullet);
+            this.fire();
         }
+    };
+
+    MachineGun.prototype.fire = function (event) {
+        if (this.fireDelay < Constants.MACHINE_FIRE_DELAY) {
+            return;
+        }
+        var posOnShip = this.gun.localToGlobal(this.gun.x + Constants.PIECE_WIDTH / 2, this.gun.y - Constants.PIECE_HEIGHT / 4);
+        var posOnWorld = Global.getInstance().world.globalToLocal(posOnShip.x, posOnShip.y);
+        var bullet = new BulletMachine(posOnWorld.x, posOnWorld.y, this.gun.rotation + this.parent.rotation);
+        Global.getInstance().listBullet.push(bullet);
+        Global.getInstance().world.addChild(bullet);
+        this.fireDelay = 0;
     };
 
     window.MachineGun = MachineGun;
